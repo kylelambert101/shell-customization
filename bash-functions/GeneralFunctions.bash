2651efe723c20776a53338ebba445751bc1ff2c8
Kyle <kylelambert101@gmail.com>
2022-03-22 12:48:34 -0400

    Add ffind function

--- bash-functions/GeneralFunctions.bash
+++ bash-functions/GeneralFunctions.bash
@@ -14,6 +14,22 @@ path(){
         echo "${PATH//:/$'\n'}"
     fi
 }
+
+# Wrapper for find...grep style searches which ignores node_modules and shows matching files in blue
+ffind(){
+    local DIR="$1"
+    local FILEPATTERN="$2"
+    local TARGET="$3"
+
+    local BLUE=$(tput setaf 4)
+    local NORMAL=$(tput sgr0)
+
+    find $DIR -name *node_modules* -prune -o \
+        -type f -name $FILEPATTERN \
+        -exec printf "${BLUE}{}${NORMAL}\n" \; \
+        -exec grep -ni $TARGET {} \;
+}
+
 # Kill the process(es) running on port(s) $@
 killPorts(){
     if [[ ! $(command -v "kill-port") ]]; then
